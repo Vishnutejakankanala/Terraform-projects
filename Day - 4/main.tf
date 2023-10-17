96fcb516dcabbb3bf39d549cd46cc713dff8e217
Vishnu Teja Kankanala <129249639+Vishnutejakankanala@users.noreply.github.com>
2023-10-17 13:04:13 +0530

    Update main.tf
    Signed-off-by: Vishnu Teja Kankanala <129249639+Vishnutejakankanala@users.noreply.github.com>

--- Day - 4/main.tf
+++ Day - 4/main.tf
@@ -1,1 +1,27 @@
+# Terraform remote backend configuration with S3 and DynamoDB
 
+provider "aws" {
+    region = "us-east-1" 
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-067c21fb1979f0b27"
+    instance_type = "t2.micro"
+}
+
+#create S3 bucket for terraform state file
+resource "aws_s3_bucket" "s3_bucket" {
+    bucket = "mybucket-vishnu-0" 
+}
+
+#create DynamoDB table for state locking 
+resource "aws_dynamodb_table" "terraform_lock" {
+    name = "terraform-lock"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
