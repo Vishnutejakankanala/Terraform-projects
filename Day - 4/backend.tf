ae9540d5786641726c8410291adc8246022a1e5c
Vishnu Teja Kankanala <129249639+Vishnutejakankanala@users.noreply.github.com>
2023-10-17 13:04:54 +0530

    Update backend.tf
    Signed-off-by: Vishnu Teja Kankanala <129249639+Vishnutejakankanala@users.noreply.github.com>

--- Day - 4/backend.tf
+++ Day - 4/backend.tf
@@ -1,1 +1,11 @@
+# configur terraform remote backend
 
+terraform {
+  backend "s3" {
+    bucket = "mybucket-vishnu-0"
+    key = "backend/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+    dynamodb_table = "terraform-lock" ##Configure the DynamoDB table name in your Terraform backend configuration
+  }
+}
